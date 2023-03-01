98c614b315262e986cf91127db58531c4fea9435
JamaalHarris <jamaalmalikharris@gmail.com>
2023-02-28 20:59:02 -0600

    created products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+product1 = Product.create(name: "coffee", price: 1, image_url:"https://www.tastingtable.com/img/gallery/coffee-brands-ranked-from-worst-to-best/l-intro-1645231221.jpg", description: "Sumatra blend Aribica dark roast")
+
+product2 = Product.create(name: "tea", price 2, image_url:"https://www.aicr.org/wp-content/uploads/2020/06/peppermint-tea-on-teacup-1417945.jpg", description: "grean tea from China")
