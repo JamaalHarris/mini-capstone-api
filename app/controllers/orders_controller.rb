73e9c6ada48348e510c563ec3e5178bf9f570f77
JamaalHarris <jamaalmalikharris@gmail.com>
2023-03-14 19:52:22 -0500

    adds create, show and index actions for orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,25 @@
 class OrdersController < ApplicationController
-  def create  
+  def create
     order = Orders.new(
-    user_id: params[:user_id],
-    product_id: params[:product_id],
-    quantity: params[:quantity],
-    subtotal: params[:subtotal],
-    tax: params[:tax],
-    total: params[:total]
+      user_id: params[:user_id],
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: params[:subtotal],
+      tax: params[:tax],
+      total: params[:total],
+    )
+    if order.save
+      # happy path
+      render json: order.as_json
+    else
+      # sad path
+      render json: { errors: order.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    order_id = params[:id]
+    order = Order.find_by(id: order_id)
+    render json: order.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
   delete "products/:id" => "products#destroy"
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
+  get "/orders/:id" => "orders#show"
+  get "/orders" => "orders#index"
 end
